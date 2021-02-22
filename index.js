471c26ff6fc1959c977d6298cc50de772d802a99
Roman Turner <rn.turner@live.com>
2021-02-22 12:00:00 -0800

    commit tree 8bdbddc1ba8e7dcb09ba1458fa19c0d339b0def2

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+/** 
+function multiply(num1, num2) {
+    return num1 * num2; 
+}
+
+function random() {
+ return Math.floor(Math.random()*10)
+}
+
+
+function mod(num3 , num4) {
+    return num3 % num4;
+}
+
+
+function max(...args) {
+    return Math.max(...args);
+}
+
+*/
+
+let num1 = 2, num2 = 31, num3 = 9, num4 = 5;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10);
+let mod = num3 % num4;
+let max = Math.max(2,3,4,5,20);
